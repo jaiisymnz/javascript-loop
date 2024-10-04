3be5ad418a5c4d8e661c043c70de703ba07791f2
Yurapon Tinlapat <you@example.com>
2024-10-04 13:25:55 +0700

    edit  ex3, ex6

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,9 @@
 let scores = [100, 20, 3, 1000];
 let minScore = scores[0];
 // Start coding here
-for (let value of scores) {if (value > minScore) {minScore = value};};
+for (let value of scores) {
+  if (value < minScore) {
+    minScore = value;
+  }
+}
 console.log(minScore);
