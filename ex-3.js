649449589b16b87880ec7c60e52c559b1e6c6c4a
Thammaruthai <thammaruthai@gmail.com>
2024-10-04 13:47:58 +0700

    feat: answer JS Loop ex1 to ex6

--- ex-3.js
+++ ex-3.js
@@ -3,4 +3,12 @@ let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
 
+
+for( let i in scores){
+    if(minScore==undefined) minScore=scores[i];
+    if(scores[i]<minScore) minScore=scores[i];
+}
+
+
+
 console.log(minScore);
--- ex-4.js
+++ ex-4.js
@@ -2,5 +2,9 @@
 let restaurants = ["KFC", "MOMO PARADISE", "Nabezo"];
 let newRestaurants = [];
 // Start coding here
+for(let i in restaurants){
+    newRestaurants[i]= "Restaurant Name: "+restaurants[i]
+}
+
 
 console.log(newRestaurants);
